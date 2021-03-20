b9cbf95d0d94cd3b2b15b7f299fecc430d274d97
Erick555 <37542552+Erick555@users.noreply.github.com>
2021-03-20 12:26:42 +0000

    Unset flags for ffmpeg build

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
+CFLAGS= CXXFLAGS= ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
 
 . /usr/lib/sdk/node12/enable.sh
 . /usr/lib/sdk/openjdk11/enable.sh
