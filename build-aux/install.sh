d1a4eb712a0da8969eb5b25bed726899f8d5b220
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-04-16 09:24:54 -0300

    Install swiftshader subdirectory
    Spotted when trying to run Chromium with Ozone/Wayland and passing
    --use-gl=desktop. Chromium fails to load with:
    
    ```
    Failed to load GLES library: /app/chromium/swiftshader/libGLESv2.so
    ```

--- build-aux/install.sh
+++ build-aux/install.sh
@@ -4,7 +4,7 @@ mkdir -p /app/chromium
 
 pushd out/Release
 # Keep file names in sync with build_devel_flatpak.py
-for path in chrome icudtl.dat *.so *.pak *.bin *.png locales MEIPreload; do
+for path in chrome icudtl.dat *.so *.pak *.bin *.png locales MEIPreload swiftshader; do
     cp -rv $path /app/chromium
 done
 popd
