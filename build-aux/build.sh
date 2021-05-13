2564f57f258d3b6d0e3a70ce33bdfecf7887a291
rany <ranygh@riseup.net>
2021-05-13 14:30:47 +0300

    Fix llvm12 thinlto bug

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
-
 . /usr/lib/sdk/node12/enable.sh
 . /usr/lib/sdk/openjdk11/enable.sh
 . /usr/lib/sdk/llvm12/enable.sh
+ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
 ninja -C out/Release -j$FLATPAK_BUILDER_N_JOBS chrome
