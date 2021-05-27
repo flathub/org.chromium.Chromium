63f22bbede2760369ffcd9253d7e52ac65fc89c9
Ryan Gonzalez <rymg19@gmail.com>
2021-05-26 20:48:53 -0500

    Try downgrading back to LLVM 11

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -2,6 +2,6 @@
 
 . /usr/lib/sdk/node12/enable.sh
 . /usr/lib/sdk/openjdk11/enable.sh
-. /usr/lib/sdk/llvm12/enable.sh
+. /usr/lib/sdk/llvm11/enable.sh
 ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
 ninja -C out/Release -j$FLATPAK_BUILDER_N_JOBS chrome
