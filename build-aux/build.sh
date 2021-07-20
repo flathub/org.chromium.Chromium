8783eac96d766bac8def1d6c6f68630417c1fd7d
Ryan Gonzalez <rymg19@gmail.com>
2021-07-20 11:48:10 -0500

    test

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -2,6 +2,8 @@
 
 ln -s /usr/lib/sdk/node12 third_party/node/linux/node-linux-x64
 ln -s /usr/lib/sdk/openjdk11 third_party/jdk/current
+ls -Rf third_party/jdk
+ls third_party/jdk/current/bin/java
 
 #. /usr/lib/sdk/node12/enable.sh
 #. /usr/lib/sdk/openjdk11/enable.sh
