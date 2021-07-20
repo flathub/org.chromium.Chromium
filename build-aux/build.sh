1072b2a0cf549c376104ff937d50c46b936758d2
Ryan Gonzalez <rymg19@gmail.com>
2021-07-20 11:09:36 -0500

    test

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-. /usr/lib/sdk/node12/enable.sh
-. /usr/lib/sdk/openjdk11/enable.sh
+ln -s /usr/lib/sdk/node12 third_party/node/linux/node-linux-x64
+ln -s /usr/lib/sdk/openjdk11 third_party/jdk/current
+
+#. /usr/lib/sdk/node12/enable.sh
+#. /usr/lib/sdk/openjdk11/enable.sh
 ninja -C out/ReleaseFree -j$FLATPAK_BUILDER_N_JOBS libffmpeg.so
 ninja -C out/Release -j$FLATPAK_BUILDER_N_JOBS chrome
