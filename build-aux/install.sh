b23c6ac6a66e323001ea71c7a85a0321ab36a419
Will Thompson <will@willthompson.co.uk>
2021-10-05 13:54:45 +0100

    Revert "Use Cobalt to start Chromium"
    This reverts commit 8b571b2d326963bfcaa188e738888c6c995ebedf.

--- build-aux/install.sh
+++ build-aux/install.sh
@@ -15,7 +15,7 @@ install -Dm 755 out/Release/libffmpeg.so /app/chromium/nonfree-codecs/lib/libffm
 for size in 24 48 64 128 256; do
     install -Dvm 644 chrome/app/theme/chromium/product_logo_$size.png /app/share/icons/hicolor/${size}x${size}/apps/org.chromium.Chromium.png;
 done
-install -Dvm 644 cobalt.ini -t /app/etc
+install -Dvm 644 portal_error.txt -t /app/share/flatpak-chromium
 install -Dvm 644 org.chromium.Chromium.desktop -t /app/share/applications
 install -Dvm 644 org.chromium.Chromium.metainfo.xml -t /app/share/metainfo
 install -Dvm 755 chromium.sh /app/bin/chromium
--- chromium.sh
+++ chromium.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+get_int32_property() {
+  gdbus call -e \
+    -d org.freedesktop.portal.Flatpak \
+    -o /org/freedesktop/portal/Flatpak \
+    -m org.freedesktop.DBus.Properties.Get \
+    org.freedesktop.portal.Flatpak "$1" \
+    | awk 'match($0, /uint32 ([0-9]+)/, m){print m[1];}'
+}
+
 merge_extensions() {
   (
     shopt -s nullglob
@@ -20,6 +29,23 @@ unlink_profiles() {
   fi
 }
 
+# Check the portal version & make sure it supports expose-pids.
+if [[ $(get_int32_property version) -lt 4 || \
+      $(($(get_int32_property supports) & 1)) -eq 0 ]]; then
+  zenity --info --title='Chromium Flatpak' --no-wrap \
+    --text="$(< /app/share/flatpak-chromium/portal_error.txt)"
+  exit 1
+fi
+
+# Merge the flags.
+if [[ -f "$XDG_CONFIG_HOME/chromium-flags.conf" ]]; then
+  IFS=$'\n'
+  flags=($(grep -v '^#' "$XDG_CONFIG_HOME/chromium-flags.conf"))
+  unset IFS
+
+  set -- "${flags[@]}" "$@"
+fi
+
 if [[ ! -f /app/chromium/extensions/no-mount-stamp ]]; then
   # Merge all legacy extension points if the symlinks had a tmpfs mounted over
   # them.
@@ -31,4 +57,8 @@ fi
 # Unlink any profiles from the sync keys to avoid any expected deletions.
 unlink_profiles
 
-exec cobalt "$@"
+flextop-init
+
+export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
+export CHROME_WRAPPER=$(readlink -f "$0")
+exec /app/chromium/chrome "$@"
